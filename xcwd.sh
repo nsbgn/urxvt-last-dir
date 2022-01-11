effadc54c7da74c558c8c4c56569a7ff39a159d7
nsv <nsv@valk.ws>
2022-01-11 12:45:03 +0100

    xcwd.sh returns `$HOME` for non-terminal windows.

--- xcwd.sh
+++ xcwd.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-
 # Returns the current working directory of the focused terminal window. Works
-# for urxvtc.
+# for urxvtc and takes into account foreground process.
 # Inspired by:
 # - https://github.com/wknapik/lastcwd
 # - https://gist.github.com/viking/5851049
 
-if id=$(xdotool getactivewindow); then
-    readlink /proc/$(ps e -o tpgid=,pid=,args= \
-    | grep -m 1 WINDOWID=$id | tr -s ' ' | cut -f2 -d' ')/cwd
-else
-    echo $HOME
-fi
+readlink /proc/$(ps e -o tpgid=,tty=,args= \
+    | awk -F' ' '$2~/pts/ && /WINDOWID='$(xdotool getactivewindow)'/ {print $1}' \
+    | head -n1)/cwd || echo $HOME
